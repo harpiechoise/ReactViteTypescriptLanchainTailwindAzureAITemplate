30202e4f6021095202ad6834b9d799a5854602aa
glitchMaker <jcrispis57@gmail.com>
2025-01-10 15:47:17 -0300

    Azure OpenAI Sample App File

--- AzureCredentials.ts
+++ AzureCredentials.ts
@@ -3,9 +3,20 @@ import { AzureChatOpenAI } from "@langchain/openai";
 const AZURE_OPENAI_API_ENDPOINT = import.meta.env.VITE_AZURE_OPENAI_API_ENDPOINT;
 const AZURE_OPENAI_API_KEY = import.meta.env.VITE_AZURE_OPENAI_API_KEY;
 const AZURE_OPENAI_API_DEPLOYMENT_NAME = import.meta.env.VITE_AZURE_OPENAI_API_DEPLOYMENT_NAME;
+const AZURE_OPENAI_API_VERSION = import.meta.env.VITE_AZURE_OPENAI_API_VERSION;
 
-export const model = new AzureChatOpenAI({
-    azureOpenAIEndpoint: AZURE_OPENAI_API_ENDPOINT,
-    azureOpenAIApiKey: AZURE_OPENAI_API_KEY,
-    azureOpenAIApiDeploymentName: AZURE_OPENAI_API_DEPLOYMENT_NAME,
-})
+export function modelMaker(temperature: number,
+     maxTokens: number, 
+     timeout: number, 
+     maxRetries: number) {
+    return new AzureChatOpenAI({
+        azureOpenAIEndpoint: AZURE_OPENAI_API_ENDPOINT,
+        azureOpenAIApiKey: AZURE_OPENAI_API_KEY,
+        azureOpenAIApiDeploymentName: AZURE_OPENAI_API_DEPLOYMENT_NAME,
+        azureOpenAIApiVersion: AZURE_OPENAI_API_VERSION,
+        temperature: temperature,
+        maxTokens: maxTokens,
+        timeout: timeout,
+        maxRetries: maxRetries
+    })
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import { HumanMessage, SystemMessage, AIMessageChunk } from '@langchain/core/messages'
+import {modelMaker} from '../AzureCredentials.ts'
 type Props = {};
 
 export default function App({}: Props) {
-  return <div>App</div>;
+  const [messages, setMessages] = useState(new AIMessageChunk(''));
+  const [model] = useState(modelMaker(0.5, 100, 10000, 3));
+
+  useEffect(() => {
+    const fetchMessages = async () => {
+      const messages = [
+        new SystemMessage("Translate the following from English into Italian"),
+        new HumanMessage("hi!"),
+      ];
+    
+      let result = await model.invoke(messages)
+      setMessages(result)
+    };
+
+    fetchMessages();
+  }, []);
+  return <div>
+    {messages.content.toString()}
+  </div>;
 }
 
